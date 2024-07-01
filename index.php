2775f7c261c8aa98e90aa3e16fbe12be61161622
Arm018 <arm0182626@gmail.com>
2024-07-01 13:53:44 +0400

    PDO styled

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $user_password = password_hash($_POST['password'],PASSWORD_BCRYPT);
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+
+
+    $user_password = password_hash($password, PASSWORD_BCRYPT);
 
     $db_name = "User";
     $localhost = "localhost";
     $root = 'root';
     $password = '';
 
-    $conn = mysqli_connect($localhost, $root, $password, $db_name);
-    if (!$conn)
-    {
-        echo "Error: " . mysqli_connect_error();
+    try {
+
+        $conn = new PDO("mysql:host=$localhost;dbname=$db_name", $root, $password);
+
+        $stmt = $conn->prepare("INSERT INTO user_info (name, email, password) VALUES (:name, :email, :password)");
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $user_password);
+
+        $stmt->execute();
+
+        echo "You have successfully registered!";
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-
-    $sql = mysqli_query($conn,"INSERT INTO user_info (name, email, password) VALUES ('$name', '$email', '$user_password')");
-
-    echo "You have successfully registered!";
-
+    ?>
